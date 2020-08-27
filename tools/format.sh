c03c088f4d966df366a9f48238a4eb51976a16f3
Stephan Vedder <vedder@mbits.info>
2020-08-27 09:40:54 +0200

    Change modules extension

--- tools/format.sh
+++ tools/format.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "Formatting code"
-find kernel/ -name *.c -or -name *.cpp -or -name *.hpp | xargs clang-format -i
+find kernel/ -name *.c -or -name *.cpp -or -name *.hpp -or -name *.cppm | xargs clang-format -i
